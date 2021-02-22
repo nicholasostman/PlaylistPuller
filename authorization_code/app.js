f0e3379155d9a922c9917e6404cf154971d2a73f
Nick Ostman <nicholas.ostman@gmail.com>
2021-02-21 21:14:32 -0600

    first github ci file

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -146,5 +146,5 @@ app.get('/refresh_token', function(req, res) {
 });
 
 console.log('Listening on 8888');
-console.log('tokenz ', client_id, ' n secret ', client_secret);
+console.log('tokenz ', client_id);
 app.listen(8888);
